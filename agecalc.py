b523108767f68ffb85967a2e66033537aac48dc8
KillerAK <46451884+KillerAK@users.noreply.github.com>
2019-01-23 15:24:57 -0800

    agecalculator

---
+++ agecalc.py
@@ -0,0 +1,12 @@
+x = int(input ("year of birth"))
+
+years = 2019 - x
+
+if years <= 18:
+    print("you are a minor")
+elif years <= 35:
+    print("you are a god damn youth")
+else:
+    print ("you are an elder")
+    
+            
